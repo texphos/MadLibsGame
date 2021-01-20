bd592d44781091be6cef089ca01b408dc80d7039
texphos <66226570+texphos@users.noreply.github.com>
2021-01-19 19:41:33 -0500

    Add files via upload

---
+++ MadLibsGame.py
@@ -0,0 +1,8 @@
+
+verb = input("Enter a verb: ")
+place = input("Enter a place: ")
+order = input("Enter an order: ")
+
+print("The cat " + verb)
+print("into a " + place)
+print("and ordered " + order)
